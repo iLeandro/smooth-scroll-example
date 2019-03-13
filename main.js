a587d7e493be2f2fe1fba93d6c0e02f16981f23f
Leandro Almeida <leandro_almeida9@hotmail.com>
2019-03-13 17:52:43 +0000

    CSS Smooth Scroll

---
+++ main.js
@@ -0,0 +1,17 @@
+// // jQuery Smooth Scroll. Works with all browsers?
+// $('navbar a').on('click', function(e) {
+//     //console.log(this.hash);
+
+//     if (this.hash !== '') {
+//         e.preventDefault();
+
+//         const hash = this.hash;
+
+//         $('html, body').animate(
+//             {
+//                 scrollTop: $(hash).offSet().top
+//         }, 
+//         800
+//         );
+//     }
+// });
